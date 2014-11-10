8521135c6f9e031dfdeb6e8f15bb01a663c3bfc5
Ben Jones <bentodjones@gmail.com>
2014-11-10 15:20:29 -0800

    "Added noisless fpm simulation and reconstruction. It does not seem to work."

--- src/noiseless_fpm_simulation.m
+++ src/noiseless_fpm_simulation.m
@@ -8,12 +8,21 @@ function [ images, pupils ] = noiseless_fpm_simulation( amplitude_file, phase_fi
 source = create_test_image(amplitude_file, phase_file);
 [full_rows, full_columns] = size(source);
 high_res_transform = fft_image(source);
-[Xgrid YGrid] = scaled_meshgrid(full_rows, full_columns, 2, 2);
+[Xgrid, YGrid] = scaled_meshgrid(full_rows, full_columns, 2, 2);
+
+% Initialize Output Arrays
+images = [];
+pupils = [];
+
 % Iterate pupils - this should be made configurable
 for x=linspace(-.5, .5, 10)
     for y=linspace(-.5,.5,10)
         pupil = circular_pupil(Xgrid, YGrid, x, y, .5);
         image = imresize(ifft_image(high_res_transform .* pupil), scale);
+        
+        % Push the new pupil and image functions onto the output arrays
+        images = cat(3, images, image);
+        pupils = cat(3, pupils, pupil);
     end
 end
 
